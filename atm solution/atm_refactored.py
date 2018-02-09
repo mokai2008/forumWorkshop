35c47d367dc2aa82af27cbcb644c996f95b7098e
Michael Gad <mokai2008@gmail.com>
2018-02-09 21:52:47 +0200

    adding atm_refactored

---
+++ atm solution/atm_refactored.py
@@ -0,0 +1,35 @@
+def withdraw(balance , request):
+    print "current balance is " + str(balance)
+    if request > balance :
+        print "You don't have enough money"
+    if request <= 0 :
+        return  str(500 - request)
+        
+    else :
+          remainder = balance - request
+          while request > 0:
+
+            if request >= 100:
+                request -= 100
+                print("give 100")
+
+            elif request >= 50:
+                request -= 50
+                print("give 50")
+
+            elif request >= 10:
+                request -= 10
+                print("give 10")
+
+            elif request >= 5:
+                request -= 5
+                print("give 5")
+
+            elif request < 5:
+                print("give " + str(request))
+                request = 0
+    return remainder
+
+balance = withdraw(500,300)
+balance = withdraw(balance , 150)
+balance = withdraw(balance,100)

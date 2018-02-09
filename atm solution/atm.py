574786437f3d759b8cc9e24762e6c5e85b65ed59
Michael Gad <mokai2008@gmail.com>
2018-02-09 21:08:14 +0200

    using if-else statements instead of many while lopp

--- atm solution/atm.py
+++ atm solution/atm.py
@@ -3,23 +3,32 @@ request = int(raw_input("You have $500 : Enter the amount of money to withdraw $
 def withdraw(x):
     if x > 500 :
         print "You don't have enough money"
+    if x <= 0 :
+        return  str(500 - x)
+        
     else :
-        while x > 99 :
-            print "give 100"
-            x = x -100
-        while x > 49 :
-            print "give 50"
-            x = x -50
-        while x > 9 :
-            print "give 10"
-            x = x -10
-        while x > 4 :
-            print "give 5"
-            x = x -5
-        while x > 0 :
-            print "give " + str(x)
-            x = 0
-        return "You have got all of your money requested"
+         while x > 0:
 
-print withdraw(request)
+            if x >= 100:
+                x -= 100
+                print("give 100")
+
+            elif x >= 50:
+                x -= 50
+                print("give 50")
+
+            elif x >= 10:
+                x -= 10
+                print("give 10")
+
+            elif x >= 5:
+                x -= 5
+                print("give 5")
+
+            elif x < 5:
+                print("give " + str(request))
+                x = 0
+    return "You have got all of your money requested"
+
+withdraw(request)
 print "The amount available in your account is " + str(money - request)

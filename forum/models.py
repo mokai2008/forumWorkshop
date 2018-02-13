3a9f3347a53ef073ce48b1003c51644428312d16
Michael Gad <mokai2008@gmail.com>
2018-02-14 00:01:25 +0200

    adding main classes of members and posts

---
+++ forum/models.py
@@ -0,0 +1,12 @@
+class member :
+    def __init__ (self, name, age) :
+        self.name = name
+        self.age = age
+    
+class post :
+    def __init__ (self, title, published_date, author_name , content) :
+        self.title = title
+        self.published_date = published_date
+        self.author_name = author_name
+        self.content = content
+
